90018a05460df052f13ae4bf402ad8f9f38f30bd
eng-hijji <mohammedhijji984@gmail.com>
2026-03-22 14:22:17 +0200

    first commit

---
+++ pass.js
@@ -0,0 +1,21 @@
+const but=document.querySelector('.gen');
+const pass=document.querySelector('.pass');
+but.addEventListener('.click',()=>{
+pass.innerContent=generate();
+})
+function generate(){
+  let pass='';
+
+  return pass;
+}
+function random(){
+  return char;
+}
+function randomUpper(){
+
+}
+function randomLower(){}
+function randomSpecial(){}
+function randomDigit(){
+  
+}
