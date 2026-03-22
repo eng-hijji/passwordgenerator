4db501ae8e21c3ccee75f45ee11cba880cc51c97
eng-hijji <mohammedhijji984@gmail.com>
2026-03-22 15:15:46 +0200

    first commit

--- pass.js
+++ pass.js
@@ -1,21 +1,68 @@
 const but=document.querySelector('.gen');
-const pass=document.querySelector('.pass');
-but.addEventListener('.click',()=>{
-pass.innerContent=generate();
-})
-function generate(){
-  let pass='';
 
-  return pass;
+const pass=document.querySelector('.pass');
+but.addEventListener('click',()=>{
+
+pass.innerText=generate();
+
+})
+
+function generate(){
+  let passw='';
+  for(let i=0;i<8;i++){
+    passw+=random();
+  }
+  return passw;
 }
 function random(){
+let num = Math.floor(Math.random() * 4) + 1;
+let char='';
+if(num==1){
+char=randomUpper();
+}
+else if(num==2){
+  char=randomLower();
+}
+else if(num==3){
+  char=randomDigit();
+}
+else if(num==4){
+  char=randomSpecial()
+}
   return char;
 }
+
+
+
+
+
+
+
 function randomUpper(){
+let upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let index=Math.floor(Math.random()*26);
+return upper[index];
+}
+
+
+
+
+
+function randomLower(){
+  let lower='abcdefghijklmnopqrstuvwxyz'
+  let index=Math.floor(Math.random()*26);
+  return lower[index];
+}
+
+function randomSpecial(){
+  let specials = "!@#$%^&*()_+[]{}|:,.<>?~";
+
+  let index=Math.floor(Math.random()*24);
+  return specials[index];
 
 }
-function randomLower(){}
-function randomSpecial(){}
+
 function randomDigit(){
-  
+return Math.floor(Math.random()*10);
 }
+console.log(generate());
